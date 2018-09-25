fd10377d7a3b968785c0d9196d589730f4a87c25
Patricio de la Guardia <padlgt01@gmail.com>
2018-09-25 19:32:48 -0400

    Create queryParser util for parsing query objects

---
+++ utils/queryParser.js
@@ -0,0 +1,14 @@
+function queryParser(url) {
+	let queryStartIndex = url.indexOf('?');
+	let query = url.substring(queryStartIndex+1);
+	let queryArray = query.split('&');
+	let queryObj = {};
+	queryArray.forEach((el) => {
+		el = el.split('=');
+		queryObj[el[0]] = el[1];
+	});
+	return queryObj;
+}
+
+module.exports = queryParser;
+
